d661a767cc07bf84452918cdbf2caef638f4f664
Henry Ohanga <ohanga.henry@gmail.com>
2019-05-22 19:43:14 +0300

    ACT-45 update response formating

--- activity/views.py
+++ activity/views.py
@@ -38,8 +38,6 @@ def index(request, selected_countries=None, id=0, sector=0):
     if request.method == 'POST' and request.is_ajax:
         data = request.POST
 
-        print(data)
-
         program = Program(name=data.get(
             'program_name'), start_date=data.get('start_date'), end_date=data.get('end_date'))
 
@@ -50,7 +48,7 @@ def index(request, selected_countries=None, id=0, sector=0):
             program.sector.set(sectors)
             
             # Return a "created" (201) response code.
-            return HttpResponse(content={'success': True, 'program': program})
+            return HttpResponse(program)
         except Exception as ex:
             raise Exception(ex)
 
