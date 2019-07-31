f12ad68e1ebbdad4828ca4474bbac39b1cedc82d
William Muli <willi.wambu@gmail.com>
2019-07-31 09:48:02 +0300

    ACT-155 Add url for POSTing new documentation

--- workflow/urls.py
+++ workflow/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('objectives', objectives_list, name='objectives'),
     path('objectives/tree', objectives_tree, name='objectives-tree'),
     path('level2/add', add_level2, name='add-level2'),
+    path('documentation/add', add_documentation, name='add-documentation'),
 
     re_path(r'^level2/project/(?P<pk>\w+)/$',
             ProjectDash.as_view(), name='project_dashboard'),
