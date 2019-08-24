b6b1aa24677151d717e50f521b8e74ea578c82db
hegemony <ohanga.henry@gmail.com>
2019-08-24 16:52:59 +0300

    ACT-216 add quick entry modal for the training

--- formlibrary/urls.py
+++ formlibrary/urls.py
@@ -9,6 +9,7 @@ from django.urls import path
 urlpatterns = [
     path('training_list/<slug:program>/<slug:project>/', TrainingList.as_view(),
          name='training_list'),
+    path('add-training', add_training, name='add_training'),
     path('training_objects/<slug:program>/<slug:project>/',
          TrainingListObjects.as_view(), name='training_objects'),
     path('training_add/<slug:id>/', TrainingCreate.as_view(),
