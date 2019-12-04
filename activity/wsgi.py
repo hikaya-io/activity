3d0c73e31d57f0d33f4fd33cca450eb51deb9a71
Peter Odeny <odenypeter@gmail.com>
2019-12-04 08:52:30 +0300

    fix dev deployment issues

--- activity/wsgi.py
+++ activity/wsgi.py
@@ -24,18 +24,18 @@ from sys import path
 SITE_ROOT = dirname(dirname(abspath(__file__)))
 path.append(SITE_ROOT)
 
-debug = os.environ.get('DEBUG', False)
-
-if debug is False:
-    settings = "activity.settings.production"
-else:
-    settings = "activity.settings.local"
+# debug = os.environ.get('DEBUG', False)
+#
+# if debug is False:
+#     settings = "activity.settings.production"
+# else:
+#     settings = "activity.settings.local"
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "jajaja.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "activity.settings.local")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
