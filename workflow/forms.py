117c1d8cf0adb6a56baf98794da5b39c16e5fa9d
Peter Odeny <odenypeter@gmail.com>
2019-12-12 08:27:14 +0300

    ACT-395:- populate site columns

--- workflow/forms.py
+++ workflow/forms.py
@@ -328,67 +328,67 @@ class ProjectAgreementSimpleForm(forms.ModelForm):
     documentation_government_approval = forms.FileField(required=False)
     documentation_community_approval = forms.FileField(required=False)
 
-    effect_or_impact = forms.CharField(
-        label="Relation to Project Logic",
-        help_text="Please do not include outputs and keep less than 120 "
-                  "words. Describe the logic that will link this "
-                  "project/activity to the proposed desired outcome/goal. "
-                  "Note any assumptions that are critical in this logic "
-                  "chain.", widget=forms.Textarea,
-        required=False)
-    justification_background = forms.CharField(
-        help_text="As someone would write a background and problem statement "
-                  "in a proposal, this should be described here. What is "
-                  "the situation in this community where the project is "
-                  "proposed and what is the problem facing them that this "
-                  "project will help solve",
-        widget=forms.Textarea, required=False)
-    justification_description_community_selection = forms.CharField(
-        help_text="How was this community selected for this project. "
-                  "It may be it was already selected as part of the project "
-                  "(like CDP-2, KIWI-2), but others may need to describe,"
-                  " out of an entire cluster, why this community? This can't "
-                  "be just 'because they wanted it', or "
-                  "'because they are poor.' It must refer to a needs "
-                  "assessment, some kind of selection criteria, maybe "
-                  "identification by the government, or some formal process.",
-        widget=forms.Textarea,
-        required=False)
-    description_of_project_activities = forms.CharField(
-        help_text="Briefly describe the day to day work you plan to complete "
-                  "in order to accomplish this project. Include rationale "
-                  "for budget, scope, timeframe as well as staff and "
-                  "stakeholders that will be necessary to seeing this "
-                  "project is effectively implemented. Site any "
-                  "documentation/monitoring efforts that you'll need to do "
-                  "before completion.",
-        widget=forms.Textarea, required=False)
-    description_of_government_involvement = forms.CharField(
-        help_text="This is an open-text field for describing the project. "
-                  "It does not need to be too long, but this is where you "
-                  "WILL be the main description and the main description that "
-                  "will be in the database. Please make this a description "
-                  "from which someone can understand what this project is "
-                  "doing. You do not need to list all activities, such as "
-                  "those that will appear on your benchmark list. Just "
-                  "describe what you are doing. You should attach technical "
-                  "drawings, technical appraisals, bill of quantity or any "
-                  "other appropriate documentation",
-        widget=forms.Textarea, required=False)
-    documentation_government_approval = forms.CharField(
-        help_text="Check the box if there IS documentation to show government "
-                  "request for or approval of the project. This should be "
-                  "attached to the proposal, and also kept in "
-                  "the program file.",
-        widget=forms.Textarea, required=False)
-    description_of_community_involvement = forms.CharField(
-        help_text="How the community is involved in the planning, approval,"
-                  " or implementation of this project should be described. "
-                  "Indicate their approval (copy of a signed MOU, or their "
-                  "signed Project Prioritization request, etc.). But also "
-                  "describe how they will be involved in the implementation "
-                  "- supplying laborers, getting training, etc.",
-        widget=forms.Textarea, required=False)
+    # effect_or_impact = forms.CharField(
+    #     label="Relation to Project Logic",
+    #     help_text="Please do not include outputs and keep less than 120 "
+    #               "words. Describe the logic that will link this "
+    #               "project/activity to the proposed desired outcome/goal. "
+    #               "Note any assumptions that are critical in this logic "
+    #               "chain.", widget=forms.Textarea,
+    #     required=False)
+    # justification_background = forms.CharField(
+    #     help_text="As someone would write a background and problem statement "
+    #               "in a proposal, this should be described here. What is "
+    #               "the situation in this community where the project is "
+    #               "proposed and what is the problem facing them that this "
+    #               "project will help solve",
+    #     widget=forms.Textarea, required=False)
+    # justification_description_community_selection = forms.CharField(
+    #     help_text="How was this community selected for this project. "
+    #               "It may be it was already selected as part of the project "
+    #               "(like CDP-2, KIWI-2), but others may need to describe,"
+    #               " out of an entire cluster, why this community? This can't "
+    #               "be just 'because they wanted it', or "
+    #               "'because they are poor.' It must refer to a needs "
+    #               "assessment, some kind of selection criteria, maybe "
+    #               "identification by the government, or some formal process.",
+    #     widget=forms.Textarea,
+    #     required=False)
+    # description_of_project_activities = forms.CharField(
+    #     help_text="Briefly describe the day to day work you plan to complete "
+    #               "in order to accomplish this project. Include rationale "
+    #               "for budget, scope, timeframe as well as staff and "
+    #               "stakeholders that will be necessary to seeing this "
+    #               "project is effectively implemented. Site any "
+    #               "documentation/monitoring efforts that you'll need to do "
+    #               "before completion.",
+    #     widget=forms.Textarea, required=False)
+    # description_of_government_involvement = forms.CharField(
+    #     help_text="This is an open-text field for describing the project. "
+    #               "It does not need to be too long, but this is where you "
+    #               "WILL be the main description and the main description that "
+    #               "will be in the database. Please make this a description "
+    #               "from which someone can understand what this project is "
+    #               "doing. You do not need to list all activities, such as "
+    #               "those that will appear on your benchmark list. Just "
+    #               "describe what you are doing. You should attach technical "
+    #               "drawings, technical appraisals, bill of quantity or any "
+    #               "other appropriate documentation",
+    #     widget=forms.Textarea, required=False)
+    # documentation_government_approval = forms.CharField(
+    #     help_text="Check the box if there IS documentation to show government "
+    #               "request for or approval of the project. This should be "
+    #               "attached to the proposal, and also kept in "
+    #               "the program file.",
+    #     widget=forms.Textarea, required=False)
+    # description_of_community_involvement = forms.CharField(
+    #     help_text="How the community is involved in the planning, approval,"
+    #               " or implementation of this project should be described. "
+    #               "Indicate their approval (copy of a signed MOU, or their "
+    #               "signed Project Prioritization request, etc.). But also "
+    #               "describe how they will be involved in the implementation "
+    #               "- supplying laborers, getting training, etc.",
+    #     widget=forms.Textarea, required=False)
 
     approval = forms.ChoiceField(
         choices=APPROVALS,
@@ -432,11 +432,11 @@ class ProjectAgreementSimpleForm(forms.ModelForm):
 
         # override the site queryset to use request.user for country
         self.fields['site'].queryset = SiteProfile.objects.filter(
-            country__in=countries)
+            organizations=self.request.user.activity_user.organization)
 
         # override the stakeholder queryset to use request.user for country
         self.fields['stakeholder'].queryset = Stakeholder.objects.filter(
-            country__in=countries)
+            organization=self.request.user.activity_user.organization)
 
         if 'Approver' not in self.request.user.groups.values_list('name',
                                                                   flat=True):
