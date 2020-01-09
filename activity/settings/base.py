04b0efa41d8907ac214a709f99d8b1083b363283
Peter Odeny <odenypeter@gmail.com>
2020-01-09 08:14:21 +0300

    add 0.0.0.0 to allowed hosts

--- activity/settings/base.py
+++ activity/settings/base.py
@@ -124,7 +124,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SITE CONFIGURATION
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 # END SITE CONFIGURATION
 
 
