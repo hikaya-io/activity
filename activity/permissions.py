604af27c456739c98fd03aee811bdfbd322fb08a
Nasir Hussain <nasirhjafri@gmail.com>
2020-03-20 00:19:18 +0500

    PULE-610 Adjusted permissions

--- activity/permissions.py
+++ activity/permissions.py
@@ -1,16 +1,14 @@
 from rest_framework import permissions
 
-from workflow.models import ActivityUserOrganizationGroup
-
 
 class IsReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return not ActivityUserOrganizationGroup.objects.filter(
-            activity_user__id=int(request.user.activity_user.id),
-            organization_id=request.user.activity_user.organization.id,
-            group__name='Viewer'
-        ).exists()
+        viewer_group = request.user.activity_user.activity_user_org_group.filter(group__name='Viewer')
+        return not viewer_group.exists()
 
