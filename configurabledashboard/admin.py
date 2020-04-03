c64773db9d994df51c3cc9177b060c4f1bec1d2d
Calix Huang <47236374+calixo888@users.noreply.github.com>
2020-04-03 05:46:35 -0700

    Migrated admin models to admin.py  (#363)
    * fixed hikaya 404 link - now linking to hikaya homepage
    
    * minor changes
    
    * refactoring and failure of transforming modal trigger to vuejs
    
    * migrated customdashbaord admin models to admin.py
    
    * migrated configurabledashboard admin models to admin.py
    
    * deleted footer links
    
    * removed programLinksAdmin model

--- configurabledashboard/admin.py
+++ configurabledashboard/admin.py
@@ -3,12 +3,31 @@
 from django.contrib import admin
 
 from .models import (
-    CustomDashboard, DashboardComponent, DashboardComponentAdmin, DashboardTheme,
-    DashboardThemeAdmin, ComponentDataSource, ComponentDataSourceAdmin,
-    CustomDashboardAdmin
+    CustomDashboard, DashboardComponent, DashboardTheme, ComponentDataSource
 )
 
-admin.site.register(CustomDashboard, CustomDashboardAdmin)
-admin.site.register(DashboardTheme, DashboardThemeAdmin)
-admin.site.register(DashboardComponent, DashboardComponentAdmin)
-admin.site.register(ComponentDataSource, ComponentDataSourceAdmin)
+@admin.register(CustomDashboard)
+class CustomDashboardAdmin(admin.ModelAdmin):
+    list_display = ('dashboard_name', 'dashboard_description', 'is_public',
+                    'theme', 'color_palette', 'create_date', 'edit_date')
+    display = 'Custom Dashboard'
+
+@admin.register(DashboardTheme)
+class DashboardThemeAdmin(admin.ModelAdmin):
+    list_display = ('theme_name', 'theme_description', 'is_public',
+                    'number_of_components', 'layout_dictionary', 'create_date',
+                    'edit_date')
+    display = 'Dashboard Theme'
+
+@admin.register(DashboardComponent)
+class DashboardComponentAdmin(admin.ModelAdmin):
+    list_display = ('component_name', 'component_description',
+                    'component_type', 'data_required', 'create_date',
+                    'edit_date')
+    display = 'Dashboard Components'
+
+@admin.register(ComponentDataSource)
+class ComponentDataSourceAdmin(admin.ModelAdmin):
+    list_display = ('data_name', 'data_type', 'data_source',
+                    'data_filter_key', 'create_date', 'edit_date')
+    display = 'Data Source'
