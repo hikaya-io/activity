d9f967dda1f8e03142f912778fc898ca6940e20c
Peter Odeny <odenypeter@gmail.com>
2020-04-08 07:55:35 +0300

    display the map when a user selects the country for the first time

--- static/vue.js/organization/organization_location.js
+++ static/vue.js/organization/organization_location.js
@@ -9,45 +9,45 @@ new Vue({
 		organization: null,
 		countries: [],
 		countriesData: [],
-        country_code: '',
+		country_code: '',
 		location_description: '',
-        latitude: null,
+		latitude: null,
 		longitude: null,
 		admin_boundary: '',
 		zoom: null,
 		adminBoundaries: [
-			{label: 'ADM0', value: 'ADM0'},
-			{label: 'ADM1', value: 'ADM1'},
-			{label: 'ADM2', value: 'ADM2'},
-			{label: 'ADM3', value: 'ADM3'}
+			{ label: 'ADM0', value: 'ADM0' },
+			{ label: 'ADM1', value: 'ADM1' },
+			{ label: 'ADM2', value: 'ADM2' },
+			{ label: 'ADM3', value: 'ADM3' },
 		],
-		showAdminBoundary: false
+		showAdminBoundary: false,
 	},
-	beforeMount: function() {
+	beforeMount: function () {
 		this.makeRequest('GET', '/workflow/organization/?user_org=true')
-			.then(response => {
+			.then((response) => {
 				if (response.data) {
 					this.organization = response.data[0];
-					this.setOrganizationFields(response.data[0])
-					if(this.organization.admin_boundary) {
+					this.setOrganizationFields(response.data[0]);
+					if (this.organization.admin_boundary) {
 						this.showAdminBoundary = true;
-						this.getBoundaryData(this.organization.admin_boundary)
+						this.getBoundaryData(this.organization.admin_boundary);
 					}
 				}
 			})
-			.catch(e => {
-				toastr.error('There was a problem loading organization location from the database!');
+			.catch((e) => {
+				toastr.error(
+					'There was a problem loading organization location from the database!'
+				);
 				this.organization = null;
 			});
 		// load countries
 		this.loadCountries();
-		
 	},
 	methods: {
-
-        /**
-         * edit organization location
-         */
+		/**
+		 * edit organization location
+		 */
 		async updateLocation() {
 			try {
 				const response = await this.makeRequest(
@@ -57,15 +57,15 @@ new Vue({
 						country_code: this.country_code,
 						location_description: this.location_description,
 						latitude: this.latitude,
-						longitude: this.longitude ,
+						longitude: this.longitude,
 						zoom: this.zoom,
-						admin_boundary: this.admin_boundary
-                    }
+						admin_boundary: this.admin_boundary,
+					}
 				);
 				if (response) {
 					toastr.success('Organization location was successfully updated');
-					this.organization = response.data
-					this.setOrganizationFields(response.data)
+					this.organization = response.data;
+					this.setOrganizationFields(response.data);
 				}
 			} catch (e) {
 				toastr.error('There was a problem updating your data');
@@ -73,19 +73,19 @@ new Vue({
 		},
 
 		/**
-         * Cancel edit organization location
-         */
+		 * Cancel edit organization location
+		 */
 		cancelLocationUpdate() {
-			this.setOrganizationFields(this.organization)
+			this.setOrganizationFields(this.organization);
 		},
 
-        /**
-         * make requests for CRUD operations using axios
-         * @param { string } method - request method
-         * @param { string } url  - request url
-         * @param { string } data - request payload
-         * @return { Promise } - axios respons ePromise
-         */
+		/**
+		 * make requests for CRUD operations using axios
+		 * @param { string } method - request method
+		 * @param { string } url  - request url
+		 * @param { string } data - request payload
+		 * @return { Promise } - axios respons ePromise
+		 */
 		makeRequest(method, url, data = null) {
 			axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 			axios.defaults.xsrfCookieName = 'csrftoken';
@@ -110,47 +110,46 @@ new Vue({
 		 */
 		loadCountries() {
 			this.makeRequest('GET', '/workflow/countries')
-			.then(response => {
-				if (response.data) {
-					this.countries = response.data;
-				}
-			})
-			.catch(e => {
-				this.countries = [];
-			});
+				.then((response) => {
+					if (response.data) {
+						this.countries = response.data;
+					}
+				})
+				.catch((e) => {
+					this.countries = [];
+				});
 		},
 
 		/**
 		 * Call this function to draw thw map
 		 */
-		showTheMap(adminBoundary=null, geoJsonData=null) {
+		showTheMap(adminBoundary = null, geoJsonData = null) {
 			var container = L.DomUtil.get('org_map');
-			if(container != null){
+			if (container != null) {
 				container._leaflet_id = null;
-			} 
+			}
 			let map = L.map('org_map').setView(
-				[
-					this.latitude ? this.latitude : 0.00, 
-					this.longitude ? this.longitude : 0.00
-				], 
+				[this.latitude ? this.latitude : 0.0, this.longitude ? this.longitude : 0.0],
 				this.zoom ? this.zoom : 5
-				);
+			);
 
 			L.tileLayer(
-				'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-					'attribution': 'Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+				'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+				{
+					attribution:
+						'Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.',
 				}
 			).addTo(map);
-			
+
 			if (adminBoundary) {
 				var myStyle = {
-					"color": "#00CCCC",
-					"weight": 3,
-					"opacity": 0.50
+					color: '#00CCCC',
+					weight: 2,
+					opacity: 0.5,
 				};
-				
+
 				L.geoJSON(geoJsonData, {
-					style: myStyle
+					style: myStyle,
 				}).addTo(map);
 			}
 		},
@@ -161,6 +160,9 @@ new Vue({
 		 */
 		countryCodeSelected(value) {
 			this.showAdminBoundary = true;
+			if (this.admin_boundary) {
+				this.getBoundaryData(this.admin_boundary);
+			}
 		},
 
 		/**
@@ -171,31 +173,26 @@ new Vue({
 		},
 
 		/**
-		 * Load 
+		 * Load
 		 */
 		getBoundaryData(admin) {
 			let data = null;
-			if(this.admin_boundary && this.country_code.length) {
-				const country = this.countries.find(item => item.id === +this.country_code[0]);
+			if (this.admin_boundary && this.country_code.length) {
+				const country = this.countries.find((item) => item.id === +this.country_code[0]);
 				this.makeRequest(
-					'GET', 
-					`https://raw.githubusercontent.com/hikaya-io/admin-boundaries/master/data/${
-						country.code.toUpperCase()
-					}/${
-						admin.toUpperCase()
-					}/${country.code.toUpperCase()}_${admin.toUpperCase()}.geojson`
+					'GET',
+					`https://raw.githubusercontent.com/hikaya-io/admin-boundaries/master/data/${country.code.toUpperCase()}/${admin.toUpperCase()}/${country.code.toUpperCase()}_${admin.toUpperCase()}.geojson`
 				)
-				.then(response => {
-					data = response.data;
-					this.showTheMap(admin, data);
-				})
-				.catch(e => {
-					toastr.error('There was a problem loading boundary data for the country');
-				});
+					.then((response) => {
+						data = response.data;
+						this.showTheMap(admin, data);
+					})
+					.catch((e) => {
+						toastr.error('There was a problem loading boundary data for the country');
+					});
 			} else {
 				this.showTheMap();
 			}
-
 		},
 
 		/**
@@ -203,34 +200,34 @@ new Vue({
 		 */
 		loadBoundaryData() {
 			this.countriesData = [];
-			if(this.country_code) {
-				this.country_code.forEach(code => {
+			if (this.country_code) {
+				this.country_code.forEach((code) => {
 					let data = null;
-					const country = this.countries.find(item => item.id === +this.country_code[0]);
+					const country = this.countries.find(
+						(item) => item.id === +this.country_code[0]
+					);
 					this.makeRequest(
-						'GET', 
-						`https://raw.githubusercontent.com/hikaya-io/admin-boundaries/master/data/${
-							country.code.toUpperCase()
-						}/${
-							admin.toUpperCase()
-						}/${country.code.toUpperCase()}_${admin.toUpperCase()}.geojson`
+						'GET',
+						`https://raw.githubusercontent.com/hikaya-io/admin-boundaries/master/data/${country.code.toUpperCase()}/${admin.toUpperCase()}/${country.code.toUpperCase()}_${admin.toUpperCase()}.geojson`
 					)
-					.then(response => {
-						data = response.data;
-						this.countriesData.push(data);						
-					})
-					.catch(e => {
-						toastr.error('There was a problem loading boundary data for the country');
-					});
+						.then((response) => {
+							data = response.data;
+							this.countriesData.push(data);
+						})
+						.catch((e) => {
+							toastr.error(
+								'There was a problem loading boundary data for the country'
+							);
+						});
 				});
 			}
-		}
+		},
 	},
 
 	computed: {
-        /**
-         * Check if organization form is valid
-         */
+		/**
+		 * Check if organization form is valid
+		 */
 		isFormValid() {
 			return this.name;
 		},
