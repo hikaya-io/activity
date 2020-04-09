77692bc5696dca8c950840bf844d15badc38ef86
Nasir Hussain <nasirhjafri@gmail.com>
2020-04-09 19:28:09 +0500

    ACT-688 Fixed migrations

--- formlibrary/migrations/0002_auto_20200215_0114.py
+++ formlibrary/migrations/0002_auto_20200215_0114.py
@@ -45,22 +45,22 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='workflow.Province'),
         ),
         migrations.AddField(
-            model_name='individual',
+            model_name='beneficiary',
             name='distribution',
             field=models.ManyToManyField(blank=True, to='formlibrary.Distribution'),
         ),
         migrations.AddField(
-            model_name='individual',
+            model_name='beneficiary',
             name='program',
             field=models.ManyToManyField(blank=True, to='workflow.Program'),
         ),
         migrations.AddField(
-            model_name='individual',
+            model_name='beneficiary',
             name='site',
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='workflow.SiteProfile'),
         ),
         migrations.AddField(
-            model_name='individual',
+            model_name='beneficiary',
             name='training',
             field=models.ManyToManyField(blank=True, to='formlibrary.TrainingAttendance'),
         ),
