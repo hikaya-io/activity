4d3ccb3176009e8ac69637a8728c92e2bc4e3ed3
Calix Huang <calix.huang1@gmail.com>
2020-04-10 10:59:11 -0700

    added null=True to fund code model

--- workflow/models.py
+++ workflow/models.py
@@ -402,7 +402,7 @@ class Contact(models.Model):
 # other programs is 'Program Dashboard'
 class FundCode(models.Model):
     name = models.CharField('Fund Code', max_length=255, blank=True)
-    percentage_of_funding = models.IntegerField('% of Funding', blank=True)
+    percentage_of_funding = models.IntegerField('% of Funding', null=True, blank=True)
     stakeholder = models.ForeignKey(
         'Stakeholder', related_name='stakeholder', null=True, blank=True,
         on_delete=models.SET_NULL)
