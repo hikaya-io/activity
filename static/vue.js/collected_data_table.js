c0590c87e09ba6c2d029c7b6f97523b6f4ab6d4e
Isaac Kimaiyo <40719885+Kimaiyo077@users.noreply.github.com>
2020-04-25 15:04:54 +0300

    reset current result after update (#489)
    Co-authored-by: Andrew <andrew.tc.pham@gmail.com>

--- static/vue.js/collected_data_table.js
+++ static/vue.js/collected_data_table.js
@@ -246,6 +246,7 @@ $(document).ready(() => {
                     }
                   })
                   this.isEdit = false;
+                  this.currentResult = null;
                   this.modalHeader = 'Add Result';
                   this.toggleResultModal();
                 }
