03dc7183d9ed70f1bdb86de5b87a1883acb8ba3a
anastiour@gmail.com <anas@cedrus.ma>
2020-05-13 16:51:26 +0100

    test(activity): refactor common code in setUp function

--- activity/tests/test_forms.py
+++ activity/tests/test_forms.py
@@ -5,33 +5,30 @@ from workflow.models import Organization
 
 class FormsTestCase(TestCase):
 
-    def test_form_is_valid(self):
-        """Test form is valid """
-        org_form_data = {
+    def setUp(self):
+        self.org_form_data = {
             "name": "Test Organization Name",
             "description": "Test organization description"
         }
-        form = OrganizationEditForm(data=org_form_data)
-        self.assertTrue(form.is_valid())
-        self.assertTrue(form.use_required_attribute)
-        self.assertEqual(form.errors, {})
-        self.assertEqual(form.data, org_form_data)
+        self.form = OrganizationEditForm(data=self.org_form_data)
+
+    def test_form_is_valid(self):
+        """Test form is valid """
+        self.assertTrue(self.form.is_valid())
+        self.assertTrue(self.form.use_required_attribute)
+        self.assertEqual(self.form.errors, {})
+        self.assertEqual(self.form.data, self.org_form_data)
 
     def test_helper_info(self):
         """
         Test form helper details
         """
-        org_form_data = {
-            "name": "Test Organization Name",
-            "description": "Test organization description"
-        }
-        form = OrganizationEditForm(data=org_form_data)
-        self.assertEqual(form.helper.form_method, 'post')
-        self.assertEqual(form.helper.form_class, 'form-horizontal')
-        self.assertEqual(form.helper.label_class, 'col-sm-2')
-        self.assertEqual(form.helper.field_class, 'col-sm-6')
-        self.assertEqual(form.helper.form_error_title, 'Form Errors')
-        self.assertFalse(form.helper.error_text_inline)
-        self.assertTrue(form.helper.help_text_inline)
-        self.assertTrue(form.helper.html5_required)
-        self.assertEqual(form.helper.layout.get_field_names(), [[[0, 0], 'logo']])
+        self.assertEqual(self.form.helper.form_method, 'post')
+        self.assertEqual(self.form.helper.form_class, 'form-horizontal')
+        self.assertEqual(self.form.helper.label_class, 'col-sm-2')
+        self.assertEqual(self.form.helper.field_class, 'col-sm-6')
+        self.assertEqual(self.form.helper.form_error_title, 'Form Errors')
+        self.assertFalse(self.form.helper.error_text_inline)
+        self.assertTrue(self.form.helper.help_text_inline)
+        self.assertTrue(self.form.helper.html5_required)
+        self.assertEqual(self.form.helper.layout.get_field_names(), [[[0, 0], 'logo']])
