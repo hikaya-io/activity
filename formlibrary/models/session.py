ba755d5daa88b9828d8b9e5ce071871fb54d8739
Andrew <andrew.tc.pham@gmail.com>
2020-09-04 13:40:40 +0200

    Session migrate (#683)
    * Session/Attendance models
    
    * Reviewed changes
    
    * migrations done
    
    * newmigration
    
    * newmigration2
    
    * delete old migration + create new migration
    
    Co-authored-by: Eashan Panjwani <eashan.panjwani@wustl.edu>

---
+++ formlibrary/models/session.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .case import Individual
+from .training import Training
+
+
+class Session():
+    date = models.DateTimeField()
+    training_type = models.ForeignKey(
+        Training, null=True, blank=False, on_delete=models.SET_NULL)
+    attendees = models.ManyToManyField(Individual, through='Attendance')
+
+
+class Attendance():
+    attendee = models.ForeignKey(Individual, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    attendance = models.BooleanField(default=False)
